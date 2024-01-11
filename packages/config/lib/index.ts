96fe2715453e1b101d636eff7ea2a9805571b51d
KaKa <23028015+climba03003@users.noreply.github.com>
2024-01-11 17:10:01 +0800

    feat: config (#10)
    * feat: add config

---
+++ packages/config/lib/index.ts
@@ -0,0 +1,28 @@
+import envSchema, { type EnvSchemaData, type EnvSchemaOpt } from 'env-schema'
+import { type FastifyPluginCallback } from 'fastify'
+import FastifyPlugin from 'fastify-plugin'
+
+export function build <T = EnvSchemaData> (options: EnvSchemaOpt<T>): { env: T, plugin: FastifyPluginCallback } {
+  const env = envSchema<T>(options)
+
+  const plugin: FastifyPluginCallback = function (fastify, _, done) {
+    fastify.decorate('config', env as unknown)
+    done()
+  }
+  FastifyPlugin(plugin, {
+    fastify: '4.x',
+    name: '@kakang/fastify-config',
+    decorators: {
+      fastify: [],
+      request: [],
+      reply: []
+    },
+    dependencies: [],
+    encapsulate: false
+  })
+
+  return {
+    env,
+    plugin
+  }
+}
