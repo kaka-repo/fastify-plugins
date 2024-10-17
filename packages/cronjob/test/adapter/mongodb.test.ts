9d366fab95aff05d890e9b7b9dfa0929b310b0eb
KaKa <kaka@kakang.dev>
2024-10-17 18:39:19 +0800

    fix(cronjob): missing prepare and wrong uid compute

--- packages/cronjob/test/adapter/mongodb.test.ts
+++ packages/cronjob/test/adapter/mongodb.test.ts
@@ -1,14 +1,15 @@
-import { test } from '@kakang/unit'
 import { parseExpression } from 'cron-parser'
-import Fastify from 'fastify'
+import Fastify, { FastifyInstance } from 'fastify'
 import { MongoClient } from 'mongodb'
+import { after, before, test, TestContext } from 'node:test'
+import { setTimeout } from 'node:timers/promises'
 import { fastifyCronJob } from '../../lib'
 import { MongoDBAdapter, type MongoDBAdapterOptions } from '../../lib/adapter/mongodb'
+import { MONGODB_URL } from '../config'
 
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/?replicaSet=rs0'
 const minTickMS = 32
 const maxTickMS = 256
-const RANDOM_GAP = (maxTickMS - minTickMS) + 3072
+const RANDOM_GAP = (maxTickMS - minTickMS) + (1024 * 1.5)
 
 function createDeferredPromise (): { promise: Promise<void>, resolve: () => void, reject: () => void } {
   const promise: any = {}
@@ -19,19 +20,16 @@ function createDeferredPromise (): { promise: Promise<void>, resolve: () => void
   return promise
 }
 
-function isJob (uid: unknown, expected: string): boolean {
-  return String(uid).includes(expected)
-}
+let client: MongoClient
+let fastify: FastifyInstance
 
-test('MongoDBAdapter', async function (t) {
-  const client = new MongoClient(MONGODB_URI)
-  await client.connect()
-
-  const db = client.db('foobar')
-  const fastify = Fastify()
-
+before(async function (t) {
+  client = new MongoClient(MONGODB_URL)
+  const db = client.db('cicd')
+  fastify = Fastify()
   const adapterOption: MongoDBAdapterOptions = {
     db,
+    resetOnInit: true
   }
   await fastify.register(fastifyCronJob, {
     application: t.name,
@@ -41,167 +39,242 @@ test('MongoDBAdapter', async function (t) {
     maxTickMS,
     maxExecutionMS: 8000,
   })
+})
 
-  fastify.cronjob.on('executed', function (task) {
-    ticks[task.uid]++
-    if (timestamps[task.uid].length < 2) {
-      timestamps[task.uid].push(task.timestamp as number)
-    } else {
-      timestamps[task.uid][1] = task.timestamp as number
-    }
+after(async function () {
+  await fastify.close()
+  await setTimeout(500)
+  await client.close()
+})
 
-    const now = Date.now()
+test('properly registered', function (t: TestContext) {
+  t.plan(4)
+  t.assert.ok(fastify.cronjob)
+  t.assert.equal(typeof fastify.cronjob.setTimeout, 'function')
+  t.assert.equal(typeof fastify.cronjob.setInterval, 'function')
+  t.assert.equal(typeof fastify.cronjob.setImmediate, 'function')
+})
 
-    if (
-      (
-        isJob(task.uid, 'interval') ||
-        isJob(task.uid, 'timeout') ||
-        isJob(task.uid, 'immediate')
-      ) && timestamps[task.uid].length === 2
-    ) {
-      const expected = timestamps[task.uid][0]
-      const _from = expected - RANDOM_GAP
-      const _to = expected + RANDOM_GAP
-      const diff = now - expected
+test('timeout', async function (t: TestContext) {
+  // [setup, ...runat]
+  const tasks: Record<number, number[]> = {}
+  function setupTimeout (delay: number) {
+    const promise = createDeferredPromise()
+    const uid = `timeout-${delay}`
+    tasks[delay] = [Date.now()]
+    fastify.cronjob.setTimeout(() => {
+      tasks[delay].push(Date.now())
+      promise.resolve()
+    }, delay, uid)
 
-      if (isJob(task.uid, 'interval')) {
-        if (ticks[task.uid] >= 2) {
-          fastify.cronjob.clearInterval(task.uid as string)
-          t.equal(ticks[task.uid], 2)
-          dones[task.uid]()
-        } else {
-          timestamps[task.uid][0] = now + task.delay
-          t.equal(ticks[task.uid], 1)
-        }
-      } else if (isJob(task.uid, 'cron')) {
-        if (ticks[task.uid] >= 2) {
-          fastify.cronjob.clearInterval(task.uid as string)
-          t.equal(ticks[task.uid] >= 2, true)
-          dones[task.uid]()
-        } else {
-          timestamps[task.uid][0] = now + task.delay
-          t.equal(ticks[task.uid], 1)
-        }
-      } else if (isJob(task.uid, 'loop')) {
-        fastify.cronjob.clearInterval(task.uid as string)
-        t.equal(ticks[task.uid] >= 1, true)
-        dones[task.uid]()
-      } else {
-        fastify.cronjob.clearInterval(task.uid as string)
-        t.equal(ticks[task.uid], 1)
-        dones[task.uid]()
+    return t.test(`${delay}ms`, async function (t: TestContext) {
+      await promise.promise
+
+      // check
+      const timestamps = tasks[delay]
+      const expected = (timestamps[0] + delay)
+      const diff = timestamps[1] - expected
+      const from = expected - RANDOM_GAP
+      const to = expected + RANDOM_GAP
+      t.assert.equal(timestamps.length, 2, 'executed one time')
+      // within expected range
+      t.assert.equal(timestamps[1] >= from, true, 'within random gap lower bound')
+      t.assert.equal(timestamps[1] <= to, true, 'within random gap upper bound')
+      // within expected diff
+      t.assert.equal(diff < RANDOM_GAP, true, 'within random gap')
+    })
+  }
+
+  await Promise.all([
+    setupTimeout(384),
+    setupTimeout(512),
+    setupTimeout(640),
+    setupTimeout(768),
+    setupTimeout(778),
+    setupTimeout(788),
+    setupTimeout(789),
+    setupTimeout(800),
+    setupTimeout(801),
+    setupTimeout(802),
+  ])
+})
+
+test('interval', async function (t: TestContext) {
+  // [setup, ...runat]
+  const tasks: Record<number, number[]> = {}
+  function setupInterval (delay: number) {
+    const promise = createDeferredPromise()
+    const uid = `interval-${delay}`
+    tasks[delay] = [Date.now()]
+    fastify.cronjob.setInterval(() => {
+      tasks[delay].push(Date.now())
+      if (tasks[delay].length === 3) {
+        promise.resolve()
+        fastify.cronjob.clearInterval(uid)
       }
+    }, delay, uid)
 
-      if (!isJob(task.uid, 'loop') && !isJob(task.uid, 'cron')) {
-        t.equal(_from < now && now < _to, true)
-        t.equal(diff < RANDOM_GAP, true)
+    return t.test(`${delay}ms`, async function (t: TestContext) {
+      await promise.promise
+
+      // check
+      const timestamps = tasks[delay]
+      t.assert.equal(timestamps.length, 3, 'executed two time')
+      for (let i = 1; i < timestamps.length; i++) {
+        const expected = (timestamps[i - 1] + delay)
+        const diff = timestamps[i] - expected
+        const from = expected - RANDOM_GAP
+        const to = expected + RANDOM_GAP
+        // within expected range
+        t.assert.equal(timestamps[i] >= from, true, 'within random gap lower bound')
+        t.assert.equal(timestamps[i] <= to, true, 'within random gap upper bound')
+        // within expected diff
+        t.assert.equal(diff < RANDOM_GAP, true, 'within random gap')
       }
-    }
-  })
-
-  const dones: Record<string, () => void> = {}
-  const timestamps: Record<string, number[]> = {}
-  const ticks: Record<string, number> = {}
-
-  const checkInterval = async function (interval: number): Promise<void> {
-    const promise = createDeferredPromise()
-    const uid = await fastify.cronjob.setInterval(async () => {}, interval, '' + interval)
-    dones[uid] = promise.resolve
-    timestamps[uid] = [Date.now() + interval]
-    ticks[uid] = 0
-    await promise.promise
+    })
   }
 
-  const checkTimeout = async function (interval: number): Promise<void> {
+  await Promise.all([
+    setupInterval(384),
+    setupInterval(512),
+    setupInterval(640),
+    setupInterval(768),
+    setupInterval(778),
+    setupInterval(788),
+    setupInterval(789),
+    setupInterval(800),
+    setupInterval(801),
+    setupInterval(802),
+  ])
+})
+
+test('immediate', async function (t: TestContext) {
+  // [setup, ...runat]
+  const tasks: Record<string, number[]> = {}
+  function setupImmediate (name: string) {
     const promise = createDeferredPromise()
-    const uid = await fastify.cronjob.setTimeout(async () => {}, interval, '' + interval)
-    dones[uid] = promise.resolve
-    timestamps[uid] = [Date.now() + interval]
-    ticks[uid] = 0
-    await promise.promise
+    const uid = `immediate-${name}`
+    tasks[name] = [Date.now()]
+    fastify.cronjob.setImmediate(() => {
+      tasks[name].push(Date.now())
+      promise.resolve()
+    }, uid)
+
+    return t.test(`${name}`, async function (t: TestContext) {
+      await promise.promise
+
+      // check
+      const timestamps = tasks[name]
+      const diff = timestamps[1] - timestamps[0]
+      const from = timestamps[0] - RANDOM_GAP
+      const to = timestamps[0] + RANDOM_GAP
+      t.assert.equal(timestamps.length, 2, 'executed one time')
+      // within expected range
+      t.assert.equal(timestamps[1] >= from, true, 'within random gap lower bound')
+      t.assert.equal(timestamps[1] <= to, true, 'within random gap upper bound')
+      // within expected diff
+      t.assert.equal(diff < RANDOM_GAP, true, 'within random gap')
+    })
   }
 
-  const checkCronJob = async function (cron: string): Promise<void> {
-    const next = parseExpression(cron).next().toDate()
+  await Promise.all([
+    setupImmediate('foo'),
+    setupImmediate('bar'),
+    setupImmediate('baz'),
+    setupImmediate('hello'),
+    setupImmediate('world'),
+  ])
+})
+
+test('cronjob', async function (t: TestContext) {
+  // [setup, ...runat]
+  const tasks: Record<string, number[]> = {}
+  const expecteds: Record<string, number[]> = {}
+  function setupCronJob (name: string, gap: number) {
     const promise = createDeferredPromise()
-    const uid = await fastify.cronjob.setCronJob(async () => {}, cron, cron)
-    dones[uid] = promise.resolve
-    timestamps[uid] = [+next]
-    ticks[uid] = 0
-    await promise.promise
+    const uid = `timeout-cron-${name}`
+    tasks[name] = [Date.now()]
+    expecteds[name] = [+parseExpression(name).next().toDate()]
+    fastify.cronjob.setCronJob(() => {
+      tasks[name].push(Date.now())
+      expecteds[name].push(+parseExpression(name).next().toDate())
+      if (tasks[name].length === 3) {
+        promise.resolve()
+        fastify.cronjob.clearTimeout(uid)
+      }
+    }, name, uid)
+
+    return t.test(`${name}`, async function (t: TestContext) {
+      await promise.promise
+
+      // check
+      const timestamps = tasks[name]
+      t.assert.equal(timestamps.length, 3, 'executed two time')
+      for (let i = 1; i < timestamps.length; i++) {
+        const expected = expecteds[name][i - 1]
+        const diff = timestamps[i] - expected
+        const from = expected - RANDOM_GAP
+        const to = expected + RANDOM_GAP
+        // within expected range
+        t.assert.equal(timestamps[i] >= from, true, 'within random gap lower bound')
+        t.assert.equal(timestamps[i] <= to, true, 'within random gap upper bound')
+        // within expected diff
+        t.assert.equal(diff < RANDOM_GAP, true, 'within random gap')
+      }
+    })
   }
 
-  const checkLoopTask = async function (name: number): Promise<void> {
+  await Promise.all([
+    setupCronJob('* * * * * *', 1000),
+    setupCronJob('*/2 * * * * *', 2000),
+    setupCronJob('*/3 * * * * *', 3000),
+    setupCronJob('*/4 * * * * *', 4000),
+    setupCronJob('*/5 * * * * *', 5000),
+  ])
+})
+
+test('loop', async function (t: TestContext) {
+  // [setup, ...runat]
+  const tasks: Record<string, number[]> = {}
+  function setupLoopTask (name: string, gap: number) {
     const promise = createDeferredPromise()
-    const uid = await fastify.cronjob.setLoopTask(async () => {}, '' + name)
-    dones[uid] = promise.resolve
-    timestamps[uid] = [Date.now()]
-    ticks[uid] = 0
-    await promise.promise
+    const uid = `immediate-loop-${name}`
+    tasks[name] = [Date.now()]
+    fastify.cronjob.setLoopTask(async () => {
+      tasks[name].push(Date.now())
+      if (tasks[name].length === 4) {
+        promise.resolve()
+        fastify.cronjob.clearTimeout(uid)
+      }
+      await setTimeout(gap)
+      tasks[name].push(Date.now())
+    }, name, uid)
+
+    return t.test(`${name}`, async function (t: TestContext) {
+      await promise.promise
+
+      // check
+      const timestamps = tasks[name]
+      t.assert.equal(timestamps.length, 4, 'executed two time')
+      for (let i = 1; i < timestamps.length; i += 2) {
+        const expected = timestamps[i - 1]
+        const diff = timestamps[i] - expected
+        const from = expected - RANDOM_GAP
+        const to = expected + RANDOM_GAP
+        // within expected diff
+        t.assert.equal(diff < RANDOM_GAP, true, 'within random gap')
+        // within expected range
+        t.assert.equal(timestamps[i] >= from, true, 'within random gap lower bound')
+        t.assert.equal(timestamps[i] <= to, true, 'within random gap upper bound')
+      }
+    })
   }
 
-  t.after(async function () {
-    await fastify.close()
-    await db.dropDatabase()
-    await client.close(true)
-  })
-
-  t.test('fastify.cronjob', function (t, done) {
-    const ok: typeof t.ok = t.ok
-    ok(fastify.cronjob)
-    t.equal(typeof fastify.cronjob.setTimeout, 'function')
-    t.equal(typeof fastify.cronjob.setInterval, 'function')
-    t.equal(typeof fastify.cronjob.setImmediate, 'function')
-    done()
-  })
-
-  t.test('interval', async function () {
-    const promises = [
-      checkInterval(384),
-      checkInterval(512),
-      checkInterval(640),
-      checkInterval(768),
-      checkInterval(778),
-      checkInterval(788),
-      checkInterval(789),
-      checkInterval(800),
-      checkInterval(801),
-      checkInterval(802),
-    ]
-    await Promise.allSettled(promises)
-  })
-
-  t.test('timeout', async function () {
-    const promises = [
-      checkTimeout(384),
-      checkTimeout(512),
-      checkTimeout(640),
-      checkTimeout(768),
-      checkTimeout(778),
-      checkTimeout(788),
-      checkTimeout(789),
-      checkTimeout(800),
-      checkTimeout(801),
-      checkTimeout(802),
-    ]
-    await Promise.allSettled(promises)
-  })
-
-  t.test('cronjob', async function () {
-    const promises = [
-      checkCronJob('* * * * * *'),
-      checkCronJob('*/2 * * * * *'),
-      checkCronJob('*/3 * * * * *'),
-      checkCronJob('*/4 * * * * *'),
-      checkCronJob('*/5 * * * * *'),
-    ]
-    await Promise.allSettled(promises)
-  })
-
-  t.test('looptask', async function () {
-    const promises = [
-      checkLoopTask(384),
-    ]
-    await Promise.allSettled(promises)
-  })
+  await Promise.all([
+    setupLoopTask('1s', 1000),
+    setupLoopTask('2s', 2000),
+    setupLoopTask('3s', 3000),
+    setupLoopTask('4s', 4000),
+    setupLoopTask('5s', 5000),
+  ])
 })
